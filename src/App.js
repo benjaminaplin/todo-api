0b3229fe82e44c92f020527e6686b59aaecef270
Benjamin Aplin <benjamin.aplin@reifyhealth.com>
2022-03-29 12:46:10 -0500

    inirt

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
+import {
+  useQuery,
+  useMutation,
+  useQueryClient,
+  QueryClient,
+  QueryClientProvider,
+} from 'react-query'
 import './App.css';
 
+const fetchFilms = (context) => {
+  console.log("🚀 ~ file: App.js ~ line 12 ~ fetchFilms ~ context", context)
+  
+  return fetch('https://swapi.dev/api/films')
+    .then(res => res.json())
+    .then(res=> res.results)
+}
+
+
+const queryClient = new QueryClient()
+
 function App() {
+  const [films, setFilms] = useState([])
+
+
+ // Access the client
+ const queryClient = useQueryClient()
+ 
+ // Queries
+ const query = useQuery('films', fetchFilms)
+ console.log("🚀 ~ file: App.js ~ line 27 ~ App ~ query", query)
+
+  query.isLoading && <div>...loading films</div>
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>{query.data?.map(film => (
+         <div key={film.id}>{film.title}</div>)
+      )}</div>
     </div>
   );
 }
-
-export default App;
+const appWrapper =  () =>{
+  return  (<QueryClientProvider client={queryClient}>
+  <App/>
+  </QueryClientProvider>)};
+export default appWrapper
